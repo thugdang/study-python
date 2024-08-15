7b971ddda193cc814cd4decea15019fabf26bd4d
Dang Dong Thuc <thuc.dangdong@gic.com.vn>
2024-08-15 16:04:17 +0700

    Committer: Dang Dong Thuc <thuc.dangdong@gic.com.vn> On branch drive Your branch is up to date with 'origin/drive'. Changes to be committed: modified:   Bai12 Tr2.py modified:   Bai13 Tr2.py modified:   Bai27 Tr6.py modified:   Bai28 Tr6.py modified:   Bai42 Tr8.py modified:   Bai78 Tr15.py Untracked files: Bai1 Tr1.py Bai11 Tr2.py 	Bai15 Tr3.py 	Bai16 Tr3.py 	Bai17 Tr4.py 	Bai18 Tr4.py 	Bai19 Tr4.py 	Bai21 Tr4.py 	Bai22 Tr4.py 	Bai26a Tr5.py 	Bai3 Tr1.py 	Bai30 Tr6.py 	Bai35 Tr7.py 	Bai37 Tr7.py 	Bai38 Tr7.py 	Bai4 Tr1.py 	Bai43 Tr9.py 	Bai44 Tr9.py 	Bai5 Tr1.py 	Bai51 Tr9.py 	Bai57 Tr12.py 	Bai58 Tr 12.py 	Bai59 Tr 12.py 	Bai6 Tr1.py 	Bai60 Tr12.py 	Bai61 Tr12.py 	Bai73 Tr13.py 	Bai76 Tr14.py 	Bai77 Tr14.py 	Bai8 Tr2.py

--- Bai27 Tr6.py
+++ Bai27 Tr6.py
@@ -1,22 +1,18 @@
-n = int(input('Nhap n: '))
-
 print("Cau a:")
-print('In ra cac so tu 1 den n:')
+n = int(input('Nhap n: '))
 i = 1
 while i <= n:
-    print(i, end=' ')
+    print(i)
     i += 1
 
-print('\nCau b:')
-print(f'Cac so nho hon {n} va la boi so cua 5:')
+print('Cau b:')
 i = n
 while i > 0:
     if i % 5 == 0:
-        print(i, end=' ')
+        print(i)
     i -= 1
 
-print('\nCau c:')
-print(f'Tong cac so tu 1 den {n}: ', end=' ')
+print('Cau c:')
 i = 1
 sum = 0
 while i <= n:
@@ -25,39 +21,25 @@ while i <= n:
 print(sum)
 
 print('Cau d:')
-print(f'Tong cac so chan nho hon {n}: ', end=' ')
 i = 1
-sum = 0
-while i < n:
+while i <= n:
     if i % 2 == 0:
-        sum += i
+        print(i)
     i += 1
-print(sum)
 
 print('Cau e:')
-print(f'Cac uoc so chan cua {n}:')
 i = 1
 sum = 0
 while i <= n:
-    if n % i == 0 and i % 2 == 0:
-        print(i, end=' ')
+    if n % i == 0:
+        print(i)
         sum += i
     i += 1
-print(f'\nTong cac uoc so: {sum}')
+print(f'Tong cac uoc so: {sum}')
 
 print('Cau f:')
 i = n - 1
-if i % 2 == 0:
-    print(f'So chan gan voi {n} nhat la: {i}')
-else:
-    print(f'So chan gan voi {n} nhat la: {i - 1}')
-
-print('Cau g:')
-print(f'Uoc so le lon nhat cua {n} va nho hon {n}: ', end=' ')
-i = n - 1
 while i > 0:
-    if n % i == 0 and i % 2 != 0:
-        print(i)
-        break
-    else:
-        i -= 1
+    if n % i == 0 and i % 2 == 0:
+        print(f'Uoc gan voi n nhat va la so chan la: {i}')
+    i -= 1
--- Bai78 Tr15.py
+++ Bai78 Tr15.py
@@ -1,23 +1,2 @@
-s = 'radar'
-# cau a
-sReverse = s[::-1]
-if s == sReverse:
-    print('Chuoi doi xung!')
-else:
-    print('Chuoi bat doi xung!')
-
-# cau b
-sReverse = ''.join(reversed(s))
-if s == sReverse:
-    print('Chuoi doi xung!')
-else:
-    print('Chuoi bat doi xung!')
-
-# cau c
-sReverse = ''
-for i in range(len(s) - 1, -1, -1):
-    sReverse += s[i]
-if s == sReverse:
-    print('Chuoi doi xung!')
-else:
-    print('Chuoi bat doi xung!')
+S = 'hello'
+print(S.index())
