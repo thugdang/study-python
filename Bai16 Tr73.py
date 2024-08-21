b55863d0367b15f809e2b08999b4de8968542229
ThugDang <thucdangdong@gmail.com>
2024-08-21 16:09:34 +0700

    commit bai16 tr73

--- Bai16 Tr73.py
+++ Bai16 Tr73.py
@@ -25,37 +25,58 @@ def DocNoiDungFile_CSV(filename):
     else:
         print('Da thuc hien hoan tat')
 
-# def GhiNoiDungFileCSV(filename, listcontent):
-#     with open(filename, 'w', newline='') as csvfile:
-#         fieldnames = ['name', 'branch', 'year', 'cgpa']
-#         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-#         writer.writeheader()
-#         writer.writerows(listcontent)
+def GhiNoiDungFileCSV(filename, listcontent):
+    try:
+        with open(filename, 'a', newline='') as f:
+            f.write('\n')
+            f.write(listcontent)
+    except FileNotFoundError:
+        print('Khong tim thay tap tin %s' %filename)
+    else:
+        print('Da ghi noi dung hoan tat')
 
-def GhiListNoiDungCSV(filename):
-    hoten = str(input('Nhap ten: '))
-    mon1 = float(input('Nhap diem mon 1: '))
-    mon2 = float(input('Nhap diem mon 2: '))
-    mon3 = float(input('Nhap diem mon 3: '))
-    uutien = float(input('Nhap diem uu tien: '))
-    with open(filename, '+wt', newline='') as f:
+def NhapThongTin():
+    while True:
+        try:
+            stt = int(input('Nhap STT: '))
+            hoten = str(input('Nhap ho ten: '))
+            mon1 = float(input('Nhap diem mon 1: '))
+            mon2 = float(input('Nhap diem mon 2: '))
+            mon3 = float(input('Nhap diem mon 3: '))
+            uutien = float(input('Nhap diem uu tien: '))
+            data = (f'{stt}, {hoten}, {mon1}, {mon2}, {mon3}, {uutien}')
+            return data
+        except Exception as e:
+            print('Input khong hop le! Nhap lai!')
+
+def GhiCSVDauRot(filegoc, filedau, filerot):
+    with open(filegoc, 'rt', encoding='utf8') as f:
         csvReaderObj = csv.reader(f)
+        header = next(csvReaderObj)
         lst = list(csvReaderObj)
-        hang = 0
+        dau = f'{header[0]}, {header[1]}, {header[2]}, {header[3]}, {header[4]}, {header[5]}\n'
+        rot = f'{header[0]}, {header[1]}, {header[2]}, {header[3]}, {header[4]}, {header[5]}\n'
         for row in lst:
-            hang += 1
-        l = [hang, hoten, mon1, mon2, mon3, uutien]
-        writer = csv.DictWriter(f)
-        writer.writerow(l)
+            tong = float(row[2]) + float(row[3]) + float(row[4]) + float(row[5])
+            if tong >= 20:
+                dau += f'{row[0]}, {row[1]}, {row[2]}, {row[3]}, {row[4]}, {row[5]}\n'
+            else:
+                rot += f'{row[0]}, {row[1]}, {row[2]}, {row[3]}, {row[4]}, {row[5]}\n'
+        with open(filedau, 'w', newline='') as d:
+            d.write(dau)
+            print('Da ghi noi dung vao file: dau.csv')
+        with open(filerot, 'w', newline='') as r:
+            r.write(rot)
+            print('Da ghi noi dung vao file: rot.csv')
 
-# DocNoiDungFile_CSV('E:/Documents/Studies/Python/danhsach.csv')
-data = [
-    {'name': 'Nikhil', 'branch': 'COE', 'year': 2, 'cgpa': 9.0},
-    {'name': 'Sanchit', 'branch': 'COE', 'year': 2, 'cgpa': 9.1},
-    {'name': 'Aditya', 'branch': 'IT', 'year': 2, 'cgpa': 9.3},
-    {'name': 'Sagar', 'branch': 'SE', 'year': 1, 'cgpa': 9.5},
-    {'name': 'Prateek', 'branch': 'MCE', 'year': 3, 'cgpa': 7.8},
-    {'name': 'Sahil', 'branch': 'EP', 'year': 2, 'cgpa': 9.1}
-]
-GhiListNoiDungCSV('D:/thuc.dangdong/My Documents/Studies/Python/data.csv')
-DocNoiDungFile_CSV('D:/thuc.dangdong/My Documents/Studies/Python/data.csv')
+while True:
+    user_input = str(input('Nhap thong tin thi sinh moi? (yes/no): '))
+    if user_input.lower() in ['yes', 'y']:
+        data = NhapThongTin()
+        GhiNoiDungFileCSV('D:/thuc.dangdong/My Documents/Studies/Python/danhsach.csv', data)
+    elif user_input.lower() in ['no', 'n']:
+        break
+    else:
+        print('Input khong hop le!')
+DocNoiDungFile_CSV('D:/thuc.dangdong/My Documents/Studies/Python/danhsach.csv')
+GhiCSVDauRot('D:/thuc.dangdong/My Documents/Studies/Python/danhsach.csv', 'D:/thuc.dangdong/My Documents/Studies/Python/dau.csv', 'D:/thuc.dangdong/My Documents/Studies/Python/rot.csv')
